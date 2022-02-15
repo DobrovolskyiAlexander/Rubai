57653c521e8a68c9bb1bebd3c03cae49ba69d33a
DobrovolskyiAlexander <98882969+DobrovolskyiAlexander@users.noreply.github.com>
2022-02-15 20:13:36 +0200

    Add files via upload

---
+++ rubai.java
@@ -0,0 +1,12 @@
+package main.rubai;
+
+public class rubai {
+    public static void main(String[] args) {
+        System.out.println("                  РУБАИ");
+        System.out.println("Много я лет размышлял над жизнью земной");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                 О.Хайям");
+    }
+}
